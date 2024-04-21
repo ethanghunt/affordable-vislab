2afe2100e81dfec01331ab9424a1e1497754de07
ethanghunt <ethangh@gmail.com>
2024-04-21 16:01:24 -0400

    all good things

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,13 +1,6 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FsZHJvbiIsImEiOiJjbHRhYzltZG4wNDN1MmlsbThnOWx2aHlsIn0.XnK4cKK9Xxbt-p0RQPm75g';
-const map = new mapboxgl.Map({
-	container: 'mapbox', // container ID
-	style: 'mapbox://styles/mapbox/streets-v12', // style URL
-	center: [-84.5, 33.7], // starting position [lng, lat]
-	zoom: 9.6, // starting zoom
-  // maxBounds: [[-85.68856261783463, 33.2109980948708], [-83.10992717367886, 34.362195482591545]],
-  // minZoom: 2,
-});
 
+let hoveredPolygonId = null;
 
 function boundsFromPoints(points) {
   const bounds = new mapboxgl.LngLatBounds();
@@ -17,78 +10,88 @@ function boundsFromPoints(points) {
   return bounds;
 }
 
+// flatten nested arrays of coordinates until it is a flat array of coordinates
+function flattenCoordinates(geometry) {
+  let flat = geometry.flat(Infinity);
+  // then group by 2s to make lnglat pairs
+  flat = flat.reduce((acc, curr, i) => {
+    if (i % 2 === 0) {
+      acc.push([curr, flat[i + 1]]);
+    }
+    return acc;
+  }, []);
+  return flat;
+}
+
 function displayClusterInfo(cluster_data) {
   console.log(cluster_data)
 }
 
-// map.on('load', function() {
-//   fetch('/500k')
-//   .then(response => response.json())
-//   .then(function(data) {
-//     console.log(data)
+fetch('/neighborhoods')
+.then(response => response.json())
+.then(function(data) {
+  const houses = data.houses
+  const boundaries = data.boundaries
+  const subtracted_boundaries = data.subtracted_boundaries
 
-//     map.addSource('stat_areas', {
-//       type: 'geojson',
-//       data: data
-//     });
-//     map.addLayer({
-//       id: 'stat_areas',
-//       type: 'fill',
-//       source: 'stat_areas',
-//       paint: {
-//         'fill-color': ['get', 'color'],
-//         'fill-opacity': 0.2
-//       },
-//     });
-//   });
-// });
+  console.log("boundaries", boundaries)
+  console.log("houses", houses)
+  console.log("subtracted_boundaries", subtracted_boundaries)
 
-map.on('load', function() {
-  fetch('/neighborhoods')
-  .then(response => response.json())
-  .then(function(data) {
-    const houses = data.houses
-    const boundaries = data.boundaries
-    const subtracted_boundaries = data.subtracted_boundaries
+  // select .main-display element
+  const mainDisplay = document.getElementsByClassName('main-display')[0];
+  mainDisplay.innerHTML = `
+  <div class="data-vis-container">
+    <div class="info-panel"></div>
+    <div class="mapbox-holder">
+      <div id="mapbox"></div>
+      <div class="legend">
+        <div>Median House Prices</div>
+        <div><i class="fa-solid fa-square" style="color: rgb(246, 160, 158);"></i> < 260,000</div>
+        <div><i class="fa-solid fa-square" style="color: rgb(221, 135, 133);"></i> 260,000 - 420,000</div>
+        <div><i class="fa-solid fa-square" style="color: rgb(196, 110, 108);"></i> 420,000 - 560,000</div>
+        <div><i class="fa-solid fa-square" style="color: rgb(171, 85, 83);"></i> > 560,000</div>
+      </div>
+    </div
+  </div>
+  `
 
-    console.log("boundaries", boundaries)
-    console.log("houses", houses)
-    console.log("subtracted_boundaries", subtracted_boundaries)
+  const map = new mapboxgl.Map({
+    container: 'mapbox', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-84.5, 33.7], // starting position [lng, lat]
+    zoom: 9.6, // starting zoom
+    // maxBounds: [[-85.68856261783463, 33.2109980948708], [-83.10992717367886, 34.362195482591545]],
+    // minZoom: 2,
+  });
+
+  map.on('load', function() {
 
     map.addSource('houses', {
       type: 'geojson',
       data: houses
     });
-    map.addSource('boundaries', {
-      type: 'geojson',
-      data: boundaries
-    });
     map.addSource('subtracted_boundaries', {
       type: 'geojson',
       data: subtracted_boundaries
     });
-    // map.addLayer({
-    //   id: 'houses',
-    //   type: 'circle',
-    //   source: 'houses',
-    //   paint: {
-    //     'circle-radius': 5,
-    //     'circle-color': ['get', 'color'],
-    //     'circle-opacity': ['get', 'opacity']
-    //   },
-    // });
-    // map.addLayer({
-    //   id: 'boundaries',
-    //   type: 'line',
-    //   source: 'boundaries',
-    //   paint: {
-    //     'line-color': ['get', 'color'],
-    //     'line-width': 1.5,
-    //     'line-opacity': 0.8,
-    //     'fill-opacity': 0.2,
-    //     'fill-color': 'red'
-    //   }
-    // });
+    map.addLayer({
+      id: 'houses',
+      type: 'circle',
+      source: 'houses',
+      paint: {
+        'circle-radius': 5,
+        'circle-color': ['get', 'color'],
+        'circle-opacity': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          10.5, 0,
+          11, 1,
+
+        ]
+      },
+    });
     map.addLayer({
       id: 'subtracted_boundaries',
       type: 'line',
@@ -105,90 +108,56 @@ map.on('load', function() {
       source: 'subtracted_boundaries',
       paint: {
         'fill-color': ['get', 'color'],
-        'fill-opacity': 0.2
+        // opacity starts at 0.9 and goes to 0.4 when zoomed in
+        'fill-opacity': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          9.8, 0.9,
+          11, 0.3
+        ]
       }
     });
-    map.on('click', 'houses', (e) => {
+
+    map.on('click', 'subtracted_boundaries_fill', (e) => {
       // select info-panel attribute
+      const data_vis_container = document.getElementsByClassName('data-vis-container')[0];
+      // add class to data-vis-container
+      data_vis_container.classList.add('info-pane-open');
+
       let region_id = e.features[0].properties.region_id;
-      let lng_lat = e.features[0].geometry.coordinates;
       const info = document.getElementsByClassName('info-panel')[0];
       console.log("region_id", e.features[0].properties.region_id)
-      console.log("lng_lat", lng_lat)
-      info.innerHTML = `${e.features[0].properties.region_id}<br>${lng_lat}`;
+      const props = e.features[0].properties;
+      info.innerHTML = `
+        <div>${props.name}</div>
+        <div class="info-part">
+          <div>Housing Affordability Index</div>
+          <div>${Math.round(props.hai * 100) / 100}</div>
+        </div>
+        <div class="info-part">
+          <div>Doordash Rating</div>
+          <div>${props.doordash_rating}</div>
+        </div>
+        <div class="info-part">
+          <div>Median Price Estimate</div>
+          <div>${Math.round(props.sold_price).toLocaleString()}</div>
+        </div>
+        <div class="info-part">
+          <div>Average School Ratings</div>
+          <div>${(Math.round(props.school_rating * 100)/100).toLocaleString()}</div>
+        </div>
+      `;
+
+      // find geometry by matching region_id to subtracted_boundaries object
+      const geometry = subtracted_boundaries.features.find(feature => feature.properties.region_id === region_id).geometry;
+      const coordinates = geometry.coordinates;
+      let lnglats = flattenCoordinates(coordinates);
+      let bounds = boundsFromPoints(lnglats);
+      map.fitBounds(bounds, {
+        padding: 50
+      });
     });
+
   });
 });
-
-// map.on('load', function() {
-//   fetch('/features')
-//   .then(response => response.json())
-//   .then(function(data) {
-//     const features = data.features
-//     const cluster_data = data.cluster_data
-//     console.log("features", features)
-
-//     map.addSource('points', {
-//       type: 'geojson',
-//       data: features['points']
-//     });
-//     map.addLayer({
-//       id: 'points',
-//       type: 'circle',
-//       source: 'points',
-//       paint: {
-//         'circle-radius': 5,
-//         'circle-color': ['get', 'color']
-//       },
-//     });
-//     map.addSource('centers', {
-//       type: 'geojson',
-//       data: features['centers']
-//     });
-//     map.addLayer({
-//       id: 'centers',
-//       type: 'circle',
-//       source: 'centers',
-//       paint: {
-//         'circle-radius': 5,
-//         'circle-color': ['get', 'color']
-//       }
-//     });
-//     map.addSource('shapes', {
-//       type: 'geojson',
-//       data: features['shapes']
-//     });
-//     map.addLayer({
-//       id: 'shapes',
-//       type: 'fill',
-//       source: 'shapes',
-//       paint: {
-//         'fill-color': ['get', 'color'],
-//         'fill-opacity': 0.2
-//       }
-//     });
-//     map.addLayer({
-//       id: 'shapes_line',
-//       type: 'line',
-//       source: 'shapes',
-//       paint: {
-//         'line-color': ['get', 'color'],
-//         'line-width': 1.5,
-//         'line-opacity': 0.8
-//       }
-//     });
-
-//     map.on('click', 'shapes', (e) => {
-//       const cluster_id = e.features[0].properties.cluster;
-//       const cluster_polygon = features['shapes']
-//         .features
-//         .filter(feature => feature.properties.cluster === cluster_id)[0]
-//         .geometry
-//         .coordinates[0];
-//       map.fitBounds(boundsFromPoints(cluster_polygon), {
-//         padding: 50
-//       });
-//       displayClusterInfo(cluster_data[cluster_id]);
-//     });
-//   });
-// });
--- router.py
+++ router.py
@@ -33,6 +33,20 @@ def random_color(i = 4253766):
   r = lambda: random.randint(0, 255)
   return '#%02X%02X%02X' % (r(), r(), r())
 
+def decile_color(decile, base_rgb = (146, 60, 58)):
+  difference_interval = 30
+  if decile == None:
+    return '#ccc'
+  rgb = [int(x - (decile - 5) * difference_interval) for x in base_rgb]
+  return 'rgb' + str(tuple(rgb))
+
+def clean_region_name(region_id):
+  if region_id[:4] == 'nan_':
+    region_id = region_id[4:]
+  region_name = region_id.title()
+  region_name = region_name.replace('_', ' ')
+  return region_name
+
 @app.get("/")
 async def root():
   return FileResponse('views/index.html')
@@ -45,9 +59,22 @@ async def contact():
 @app.get('/neighborhoods')
 async def official_neighborhoods():
   
-  props = pd.read_csv('data/my_props_4.csv')
+  props = pd.read_csv('data/final/properties.csv')
   props.rename(columns={'latitude': 'lat', 'longitude': 'lng'}, inplace=True)
 
+  with open('data/final/region_stats.json', 'r') as f:
+    region_stats = json.load(f)
+  
+  for region_stat in region_stats:
+    for stat in region_stat:
+      if region_stat[stat] == 'NaN':
+        region_stat[stat] = None
+    region_stat['color'] = decile_color(region_stat['Sold Price Quartile'])
+  
+  region_stats = {
+    region_stat['region_id']: region_stat for region_stat in region_stats
+  }
+  
   point_list = []
   for i, row in props.iterrows():
     point = {
@@ -58,7 +85,7 @@ async def official_neighborhoods():
       },
       "properties": {
         "region_id": row['region_id'],
-        "color": random_color(row['region_id']),
+        "color": region_stats.get(row['region_id'], {}).get('color', '#ccc'),
         "opacity": 0.5
       }
     }
@@ -67,16 +94,29 @@ async def official_neighborhoods():
     "type": "FeatureCollection",
     "features": point_list
   }
-    
-  with open('data/final/neighborhood_boundary.geojson', 'r') as f:
-    boundaries = json.load(f)
   
   with open('data/final/neighborhood_boundary_subtracted.geojson', 'r') as f:
     subtracted_boundaries = json.load(f)
+  
+  for i, feature in enumerate(subtracted_boundaries['features']):
+    region_id = feature['properties']['region_id']
+    feature['id'] = region_id
+    additional_features = {
+      'color': region_stats[region_id]['color'],
+      'name': clean_region_name(region_id),
+      'price_level': region_stats[region_id]['Average Price Level'],
+      'doordash_rating': region_stats[region_id]['Doordash Rating'],
+      'sold_price': region_stats[region_id]['Sold Price'],
+      'hai': region_stats[region_id]['HAI'],
+      'sold_price_quartile': region_stats[region_id]['Sold Price Quartile'],
+      'doordash_rating_quartile': region_stats[region_id]['Doordash Rating Quartile'],
+      'school_rating': region_stats[region_id]['School Ratings'],
+      'school_rating_quartile': region_stats[region_id]['School Ratings Quartile'],
+    }
+    feature['properties'].update(additional_features)
 
   return {
     "houses": points,
-    "boundaries": boundaries,
     "subtracted_boundaries": subtracted_boundaries
   }
 
